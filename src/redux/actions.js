ebcf500b99b73b3e3676f8b99b91b080bb7b0bef
Philipp Prus <philipp@prcptv.ai>
2019-07-24 18:50:56 +0300

    add tests for action redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,35 +1,35 @@
-import { SET_SORT, FETCH_TRACKS, ADD_TRACK, TOGGLE_PAGE, DELETE_TRACK, FETCH_LYRICS_TRACK, CLEAR_FETCH_TRACKS } from "./actionTypes";
+import * as types from "./actionTypes";
 
 
 export const fetchTracks = (name) => ({
-  type: FETCH_TRACKS,
+  type: types.FETCH_TRACKS,
   payload: { name }
 });
 
 export const clearFetchTracks = () => ({
-  type: CLEAR_FETCH_TRACKS
+  type: types.CLEAR_FETCH_TRACKS
 })
 
 export const addTrack = (track) => ({
-  type: ADD_TRACK,
+  type: types.ADD_TRACK,
   payload: { track }
 });
 
 export const deleteTrack = (index_track) => ({
-  type: DELETE_TRACK,
+  type: types.DELETE_TRACK,
   payload: index_track
 });
 
 export const openPage = () => ({
-  type: TOGGLE_PAGE
+  type: types.TOGGLE_PAGE
 });
 
 export const setSort = (sort) => ({
-   type: SET_SORT, 
+   type: types.SET_SORT, 
    payload: { sort } 
 });
 
 export const getLyrics = (track_id) => ({
-  type: FETCH_LYRICS_TRACK,
+  type: types.FETCH_LYRICS_TRACK,
   payload: {track_id}
 })
