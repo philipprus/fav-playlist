f95e4611105eb4382c04f488695c7622d617da6c
Philipp Prus <philipp@prcptv.ai>
2019-07-25 11:19:02 +0300

    connect themeProvider

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { Routes } from "./routes/Routes";
 import { Provider } from "react-redux";
 import initStore from "./redux/store";
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+
+
 function App() {
+
+  const theme = createMuiTheme({ });
+
   return  (
     <Provider store={initStore()}>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </Provider>
     );
 }
